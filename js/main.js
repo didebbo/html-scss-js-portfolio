eb8ad40302d3f154f0c530c0b9b9830df29ced55
didebbo <gianluca.napoletano.93@gmail.com>
2021-08-22 13:25:48 +0200

    aggiunto dark mode

--- js/main.js
+++ js/main.js
@@ -1,12 +1,43 @@
-const mainNav = document.getElementById("main-nav");
-const buttonNav = document.getElementById("main-button-nav");
+// DOM Elements
+const favicon = document.querySelector("#favicon");
+const menu = document.getElementById("main-nav");
+const buttonMenu = document.getElementById("main-button-nav");
+const theme_mode_icons = document.getElementsByClassName("theme-mode-icon");
 
+// Main
+window.onload = () => {
+    theme_color();
+}
+
+// Actions
+buttonMenu.onclick = () => {
+    if (localStorage.theme === "dark") localStorage.theme = "light";
+    else localStorage.theme = "dark";
+    theme_color();
+}
+
+// Function
 const show_menu = () => {
-    mainNav.classList.add("show");
-    buttonNav.classList.add("focus");
+    menu.classList.add("show");
+    buttonMenu.classList.add("focus");
 }
 
 const hide_menu = () => {
-    mainNav.classList.remove("show");
-    buttonNav.classList.remove("focus");
+    menu.classList.remove("show");
+    buttonMenu.classList.remove("focus");
+}
+
+const theme_color = () => {
+    if (localStorage.theme === "dark") {
+        favicon.href = "images/dark-icon.png";
+        document.documentElement.classList.add("dark");
+        theme_mode_icons[0].style.display = "none";
+        theme_mode_icons[1].style.display = "inline";
+    }
+    else {
+        favicon.href = "images/icon.png";
+        document.documentElement.classList.remove("dark");
+        theme_mode_icons[0].style.display = "inline";
+        theme_mode_icons[1].style.display = "none";
+    }
 }
